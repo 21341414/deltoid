538f881bf433e8494aa249b1699953247d567dbd
21341414 <phamj@pdxeaglestudent.org>
2026-04-21 19:01:48 -0700

    Update Injector.c

--- Injector.c
+++ Injector.c
@@ -2,27 +2,26 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pwd.h>
+#include <string.h>
 
 int main() {
     struct passwd *pw = getpwuid(getuid());
     const char *homedir = pw->pw_dir;
+    char cmd[1024];
 
-    printf("[ Deltoid ] KX Launcher \n");
+    printf("\033[1;36m[Deltoid]\033[0m init [ Deltoid ]..\n");
 
-    // grant sober permission to see the .so in your home folder
-    char fs_cmd[256];
-    snprintf(fs_cmd, sizeof(fs_cmd), "flatpak override --user --filesystem=%s org.vinegarhq.Sober", homedir);
-    system(fs_cmd);
+    // grant Sober access to your home directory for the .so and scripts
+    snprintf(cmd, sizeof(cmd), "flatpak override --user --filesystem=%s org.vinegarhq.Sober", homedir);
+    system(cmd);
 
-    // set the LD_PRELOAD env variable
-    char env_cmd[512];
-    snprintf(env_cmd, sizeof(env_cmd), "flatpak override --user --env=LD_PRELOAD=%s/libdeltoid.so org.vinegarhq.Sober", homedir);
+    // set the LD_PRELOAD to point to your compiled library
+    snprintf(cmd, sizeof(cmd), "flatpak override --user --env=LD_PRELOAD=%s/libdeltoid.so org.vinegarhq.Sober", homedir);
     
-    if (system(env_cmd) == 0) {
-        printf("[+] hook successful.\n");
-        printf("[!] launch sober now to execute.\n");
+    if (system(cmd) == 0) {
+        printf("\033[1;32m[Deltoid]\033[0m hooked, open sober to start.\n");
     } else {
-        printf("[-] failed to set env variable.\n");
+        printf("\033[1;31m[Error]\033[0m could not set flatpak overrides.\n");
     }
 
     return 0;
