7d2859c28d455a2fbe686b5047522665eb79d603
21341414 <phamj@pdxeaglestudent.org>
2026-04-23 12:58:53 -0700

    Update deltoidUI.c

--- deltoidUI.c
+++ deltoidUI.c
@@ -1,53 +1,32 @@
-#include <gtk/gtk.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pwd.h>
+#include <sys/types.h>
+#include <string.h>
 
-static GtkWidget *editor;
+int main() {
+    struct passwd *pw = getpwuid(getuid());
+    const char *homedir = pw->pw_dir;
+    char cmd[512];
 
-static void on_execute(GtkButton *btn, gpointer data) {
-    GtkTextBuffer *buf = gtk_text_view_get_buffer(GTK_TEXT_VIEW(editor));
-    GtkTextIter s, e;
-    gtk_text_buffer_get_bounds(buf, &s, &e);
-    char *text = gtk_text_buffer_get_text(buf, &s, &e, FALSE);
-    FILE *f = fopen("execute.lua", "w");
-    if (f) { fputs(text, f); fclose(f); }
-    g_free(text);
-}
+    printf("\033[1;36m[ Deltoid ]\033[0m initializing stealth environment...\n");
 
-static void on_attach(GtkButton *btn, gpointer data) {
-    system("./deltoid_launcher");
-    gtk_button_set_label(btn, "attached ( success )");
-}
+    // reset flatpak to clean detection flags
+    system("flatpak override --user --reset org.vinegarhq.Sober");
 
-static void activate(GtkApplication *app, gpointer data) {
-    GtkWidget *win = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(win), "Deltoid KX");
-    gtk_window_set_default_size(GTK_WINDOW(win), 800, 400);
+    // grant filesystem access
+    snprintf(cmd, sizeof(cmd), "flatpak override --user --filesystem=%s org.vinegarhq.Sober", homedir);
+    system(cmd);
 
-    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-    gtk_window_set_child(GTK_WINDOW(win), box);
+    // set injection hook
+    snprintf(cmd, sizeof(cmd), "flatpak override --user --env=LD_PRELOAD=%s/libdeltoid.so org.vinegarhq.Sober", homedir);
+    
+    if (system(cmd) == 0) {
+        printf("\033[1;32m[ Deltoid ]\033[0m stealth hook set. launch sober now.\n");
+    } else {
+        printf("\033[1;31m[ Error ]\033[0m failed to set flatpak overrides.\n");
+    }
 
-    editor = gtk_text_view_new();
-    GtkWidget *scroll = gtk_scrolled_window_new();
-    gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scroll), editor);
-    gtk_box_append(GTK_BOX(box), scroll);
-
-    GtkWidget *btn_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
-    gtk_box_append(GTK_BOX(box), btn_box);
-
-    GtkWidget *b1 = gtk_button_new_with_label("exec");
-    g_signal_connect(b1, "clicked", G_CALLBACK(on_execute), NULL);
-    gtk_box_append(GTK_BOX(btn_box), b1);
-
-    GtkWidget *b2 = gtk_button_new_with_label("attatch");
-    g_signal_connect(b2, "clicked", G_CALLBACK(on_attach), NULL);
-    gtk_box_append(GTK_BOX(btn_box), b2);
-
-    gtk_widget_show(win);
-}
-
-int main(int argc, char **argv) {
-    GtkApplication *app = gtk_application_new("com.deltoid.kx", G_APPLICATION_DEFAULT_FLAGS);
-    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-    return g_application_run(G_APPLICATION(app), argc, argv);
+    return 0;
 }
